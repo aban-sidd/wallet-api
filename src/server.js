cc05ab1b905a8a68602753181c3d5946fe09a88a
aban-sidd <abansiddiqui4@gmail.com>
2025-10-02 15:32:01 +0500

    updated

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv"
 import { initDB } from "./config/db.js";
 import ratelimiter from "./middleware/rateLimmiter.js";
 import transactionsRoute from "./routes/transactionsRoute.js"
+import job from "./config/cron.js";
 
 
 dotenv.config();
@@ -10,12 +11,18 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+if(process.env.NODE_ENV === "production") job.start(); // start cron job only in production environment
+
 //middleware
 app.use(ratelimiter); 
 app.use(express.json());
 
 
 // Routes
+app.get("/api/health" , (req,res) => {
+    res.status(200).json({status: "OK"})
+})
+
 app.use("/api/transactions" , transactionsRoute);
 
 // DataBase
